e38ae3d5527c28d6413856fb9de85a2ceb53170c
Keiji Suzuki <zuki.ebetsu@gmail.com>
2024-02-05 09:22:10 +0900

    get humidity for bme280

--- i2c/bmp280_i2c/bmp280_i2c.c
+++ i2c/bmp280_i2c/bmp280_i2c.c
@@ -10,30 +10,32 @@
 #include "pico/binary_info.h"
 #include "pico/stdlib.h"
 
- /* Example code to talk to a BMP280 temperature and pressure sensor
+/* Example code to talk to a BMP280 temperature and pressure sensor
 
-    NOTE: Ensure the device is capable of being driven at 3.3v NOT 5v. The Pico
-    GPIO (and therefore I2C) cannot be used at 5v.
+   NOTE: Ensure the device is capable of being driven at 3.3v NOT 5v. The Pico
+   GPIO (and therefore I2C) cannot be used at 5v.
 
-    You will need to use a level shifter on the I2C lines if you want to run the
-    board at 5v.
+   You will need to use a level shifter on the I2C lines if you want to run the
+   board at 5v.
 
-    Connections on Raspberry Pi Pico board, other boards may vary.
+   Connections on Raspberry Pi Pico board, other boards may vary.
 
-    GPIO PICO_DEFAULT_I2C_SDA_PIN (on Pico this is GP4 (pin 6)) -> SDA on BMP280
-    board
-    GPIO PICO_DEFAULT_I2C_SCK_PIN (on Pico this is GP5 (pin 7)) -> SCL on
-    BMP280 board
-    3.3v (pin 36) -> VCC on BMP280 board
-    GND (pin 38)  -> GND on BMP280 board
- */
+   GPIO PICO_DEFAULT_I2C_SDA_PIN (on Pico this is GP4 (pin 6)) -> SDA on BMP280
+   board
+   GPIO PICO_DEFAULT_I2C_SCK_PIN (on Pico this is GP5 (pin 7)) -> SCL on
+   BMP280 board
+   3.3v (pin 36) -> VCC on BMP280 board
+   GND (pin 38)  -> GND on BMP280 board
+*/
 
- // device has default bus address of 0x76
+// device has default bus address of 0x76
 #define ADDR _u(0x76)
 
 // hardware registers
+#define REG_ID _u(0xD0)
 #define REG_CONFIG _u(0xF5)
 #define REG_CTRL_MEAS _u(0xF4)
+#define REG_CTRL_HUM _u(0xF2)
 #define REG_RESET _u(0xE0)
 
 #define REG_TEMP_XLSB _u(0xFC)
@@ -69,9 +71,16 @@
 #define REG_DIG_P8_MSB _u(0x9D)
 #define REG_DIG_P9_LSB _u(0x9E)
 #define REG_DIG_P9_MSB _u(0x9F)
+#define REG_DIG_H1 _u(0xA1)
+#define REG_DIG_H2_LSB _u(0xE1)
+#define REG_DIG_H2_MSB _u(0xE2)
+#define REG_DIG_H3 _u(0xE3)
+#define REG_DIG_H4 _u(0xE4)
+#define REG_DIG_H5 _u(0xE5)
 
 // number of calibration registers to be read
-#define NUM_CALIB_PARAMS 24
+#define NUM_CALIB_PARAMS1 26
+#define NUM_CALIB_PARAMS2 7
 
 struct bmp280_calib_param {
     // temperature params
@@ -89,46 +98,68 @@ struct bmp280_calib_param {
     int16_t dig_p7;
     int16_t dig_p8;
     int16_t dig_p9;
+
+    uint8_t dig_h1;
+    int16_t dig_h2;
+    uint8_t dig_h3;
+    int16_t dig_h4;
+    int16_t dig_h5;
+    int8_t dig_h6;
 };
 
 #ifdef i2c_default
 void bmp280_init() {
     // use the "handheld device dynamic" optimal setting (see datasheet)
-    uint8_t buf[2];
+    uint8_t buf[2], id;
+    uint8_t reg = REG_ID;
 
-    // 500ms sampling time, x16 filter
-    const uint8_t reg_config_val = ((0x04 << 5) | (0x05 << 2)) & 0xFC;
+    // 500ms sampling time, x16 filter = 0x90
+    const uint8_t reg_config_val = ((0x04 << 5) | (0x04 << 2)) & 0xFC;
 
     // send register number followed by its corresponding value
     buf[0] = REG_CONFIG;
     buf[1] = reg_config_val;
     i2c_write_blocking(i2c_default, ADDR, buf, 2, false);
 
-    // osrs_t x1, osrs_p x4, normal mode operation
-    const uint8_t reg_ctrl_meas_val = (0x01 << 5) | (0x03 << 2) | (0x03);
+    // osrs_h x1 = 0x01
+    const uint8_t reg_ctrl_hum_val = 0x01;
+    buf[0] = REG_CTRL_HUM;
+    buf[1] = reg_ctrl_hum_val;
+    i2c_write_blocking(i2c_default, ADDR, buf, 2, false);
+
+    // osrs_t x1, osrs_p x1, normal mode operation = 0x27
+    const uint8_t reg_ctrl_meas_val = (0x01 << 5) | (0x01 << 2) | (0x03);
     buf[0] = REG_CTRL_MEAS;
     buf[1] = reg_ctrl_meas_val;
     i2c_write_blocking(i2c_default, ADDR, buf, 2, false);
+
+    i2c_write_blocking(i2c_default, ADDR, &reg, 1, true);
+    i2c_read_blocking(i2c_default, ADDR, &id, 1, false);
+    printf("id = 0x%X\n", id);
 }
 
-void bmp280_read_raw(int32_t* temp, int32_t* pressure) {
+void bmp280_read_raw(int32_t* temp, int32_t* pressure, int32_t* humidity) {
     // BMP280 data registers are auto-incrementing and we have 3 temperature and
     // pressure registers each, so we start at 0xF7 and read 6 bytes to 0xFC
-    // note: normal mode does not require further ctrl_meas and config register writes
+    // note: normal mode does not require further ctrl_meas and config register
+    // writes
 
-    uint8_t buf[6];
+    uint8_t buf[8];
     uint8_t reg = REG_PRESSURE_MSB;
-    i2c_write_blocking(i2c_default, ADDR, &reg, 1, true);  // true to keep master control of bus
-    i2c_read_blocking(i2c_default, ADDR, buf, 6, false);  // false - finished with bus
+    i2c_write_blocking(i2c_default, ADDR, &reg, 1,
+                       true);  // true to keep master control of bus
+    i2c_read_blocking(i2c_default, ADDR, buf, 8,
+                      false);  // false - finished with bus
 
-    // store the 20 bit read in a 32 bit signed integer for conversion
-    *pressure = (buf[0] << 12) | (buf[1] << 4) | (buf[2] >> 4);
-    *temp = (buf[3] << 12) | (buf[4] << 4) | (buf[5] >> 4);
+    // store the 20/16 bit read in a 32 bit signed integer for conversion
+    *pressure = (int32_t)(buf[0] << 12) | (buf[1] << 4) | (buf[2] >> 4);
+    *temp = (int32_t)(buf[3] << 12) | (buf[4] << 4) | (buf[5] >> 4);
+    *humidity = (int32_t)(buf[6] << 8) | buf[7];
 }
 
 void bmp280_reset() {
     // reset the device with the power-on-reset procedure
-    uint8_t buf[2] = { REG_RESET, 0xB6 };
+    uint8_t buf[2] = {REG_RESET, 0xB6};
     i2c_write_blocking(i2c_default, ADDR, buf, 2, false);
 }
 
@@ -137,21 +168,36 @@ void bmp280_reset() {
 int32_t bmp280_convert(int32_t temp, struct bmp280_calib_param* params) {
     // use the 32-bit fixed point compensation implementation given in the
     // datasheet
-    
+    //static int out = 0;
     int32_t var1, var2;
-    var1 = ((((temp >> 3) - ((int32_t)params->dig_t1 << 1))) * ((int32_t)params->dig_t2)) >> 11;
-    var2 = (((((temp >> 4) - ((int32_t)params->dig_t1)) * ((temp >> 4) - ((int32_t)params->dig_t1))) >> 12) * ((int32_t)params->dig_t3)) >> 14;
+    var1 = ((((temp >> 3) - ((int32_t)params->dig_t1 << 1))) *
+            ((int32_t)params->dig_t2)) >>
+           11;
+    var2 = (((((temp >> 4) - ((int32_t)params->dig_t1)) *
+              ((temp >> 4) - ((int32_t)params->dig_t1))) >>
+             12) *
+            ((int32_t)params->dig_t3)) >>
+           14;
+/*
+    if (!out) {
+      printf("t_fine = %d\n", var1 + var2);
+      out = 1;
+    }
+*/
     return var1 + var2;
 }
 
 int32_t bmp280_convert_temp(int32_t temp, struct bmp280_calib_param* params) {
-    // uses the BMP280 calibration parameters to compensate the temperature value read from its registers
+    // uses the BMP280 calibration parameters to compensate the temperature
+    // value read from its registers
     int32_t t_fine = bmp280_convert(temp, params);
     return (t_fine * 5 + 128) >> 8;
 }
 
-int32_t bmp280_convert_pressure(int32_t pressure, int32_t temp, struct bmp280_calib_param* params) {
-    // uses the BMP280 calibration parameters to compensate the pressure value read from its registers
+int32_t bmp280_convert_pressure(int32_t pressure, int32_t temp,
+                                struct bmp280_calib_param* params) {
+    // uses the BMP280 calibration parameters to compensate the pressure value
+    // read from its registers
 
     int32_t t_fine = bmp280_convert(temp, params);
 
@@ -161,21 +207,56 @@ int32_t bmp280_convert_pressure(int32_t pressure, int32_t temp, struct bmp280_ca
     var2 = (((var1 >> 2) * (var1 >> 2)) >> 11) * ((int32_t)params->dig_p6);
     var2 += ((var1 * ((int32_t)params->dig_p5)) << 1);
     var2 = (var2 >> 2) + (((int32_t)params->dig_p4) << 16);
-    var1 = (((params->dig_p3 * (((var1 >> 2) * (var1 >> 2)) >> 13)) >> 3) + ((((int32_t)params->dig_p2) * var1) >> 1)) >> 18;
+    var1 = (((params->dig_p3 * (((var1 >> 2) * (var1 >> 2)) >> 13)) >> 3) +
+            ((((int32_t)params->dig_p2) * var1) >> 1)) >>
+           18;
     var1 = ((((32768 + var1)) * ((int32_t)params->dig_p1)) >> 15);
     if (var1 == 0) {
         return 0;  // avoid exception caused by division by zero
     }
-    converted = (((uint32_t)(((int32_t)1048576) - pressure) - (var2 >> 12))) * 3125;
+    converted =
+        (((uint32_t)(((int32_t)1048576) - pressure) - (var2 >> 12))) * 3125;
     if (converted < 0x80000000) {
         converted = (converted << 1) / ((uint32_t)var1);
     } else {
         converted = (converted / (uint32_t)var1) * 2;
     }
-    var1 = (((int32_t)params->dig_p9) * ((int32_t)(((converted >> 3) * (converted >> 3)) >> 13))) >> 12;
+    var1 = (((int32_t)params->dig_p9) *
+            ((int32_t)(((converted >> 3) * (converted >> 3)) >> 13))) >>
+           12;
     var2 = (((int32_t)(converted >> 2)) * ((int32_t)params->dig_p8)) >> 13;
-    converted = (uint32_t)((int32_t)converted + ((var1 + var2 + params->dig_p7) >> 4));
+    converted =
+        (uint32_t)((int32_t)converted + ((var1 + var2 + params->dig_p7) >> 4));
     return converted;
+}
+
+uint32_t bmp280_convert_humidity(int32_t humidity, int32_t temp,
+                                 struct bmp280_calib_param* params) {
+    int32_t t_fine = bmp280_convert(temp, params);
+
+    int32_t var1;
+
+    var1 = (t_fine - ((int32_t)76800));
+    var1 = (((((humidity << 14) - (((int32_t)params->dig_h4) << 20) -
+               (((int32_t)params->dig_h5) * var1)) +
+              ((int32_t)16384)) >>
+             15) *
+            (((((((var1 * ((int32_t)params->dig_h6)) >> 10) *
+                 (((var1 * ((int32_t)params->dig_h3)) >> 11) +
+                  ((int32_t)32768))) >>
+                10) +
+               ((int32_t)2097152)) *
+                  ((int32_t)params->dig_h2) +
+              8192) >>
+             14));
+    var1 =
+        (var1 -
+         (((((var1 >> 15) * (var1 >> 15)) >> 7) * ((int32_t)params->dig_h1)) >>
+          4));
+    var1 = (var1 < 0 ? 0 : var1);
+    var1 = (var1 > 419430400 ? 419430400 : var1);
+
+    return (uint32_t)(var1 >> 12);
 }
 
 void bmp280_get_calib_params(struct bmp280_calib_param* params) {
@@ -184,26 +265,51 @@ void bmp280_get_calib_params(struct bmp280_calib_param* params) {
     // there are 3 temperature params, and 9 pressure params, each with a LSB
     // and MSB register, so we read from 24 registers
 
-    uint8_t buf[NUM_CALIB_PARAMS] = { 0 };
-    uint8_t reg = REG_DIG_T1_LSB;
-    i2c_write_blocking(i2c_default, ADDR, &reg, 1, true);  // true to keep master control of bus
+    uint8_t buf[NUM_CALIB_PARAMS1] = {0};
+    uint8_t reg1 = REG_DIG_T1_LSB;
+    uint8_t reg2 = REG_DIG_H2_LSB;
+
+    // read 0x88 - 0xa1
+    i2c_write_blocking(i2c_default, ADDR, &reg1, 1,
+                       true);  // true to keep master control of bus
     // read in one go as register addresses auto-increment
-    i2c_read_blocking(i2c_default, ADDR, buf, NUM_CALIB_PARAMS, false);  // false, we're done reading
+    i2c_read_blocking(i2c_default, ADDR, buf, NUM_CALIB_PARAMS1,
+                      false);  // false, we're done reading
 
     // store these in a struct for later use
-    params->dig_t1 = (uint16_t)(buf[1] << 8) | buf[0];
-    params->dig_t2 = (int16_t)(buf[3] << 8) | buf[2];
-    params->dig_t3 = (int16_t)(buf[5] << 8) | buf[4];
+    params->dig_t1 = (uint16_t)(buf[1] << 8) | (uint16_t)buf[0];
+    params->dig_t2 = (int16_t)(buf[3] << 8) | (int16_t)buf[2];
+    params->dig_t3 = (int16_t)(buf[5] << 8) | (int16_t)buf[4];
 
-    params->dig_p1 = (uint16_t)(buf[7] << 8) | buf[6];
-    params->dig_p2 = (int16_t)(buf[9] << 8) | buf[8];
-    params->dig_p3 = (int16_t)(buf[11] << 8) | buf[10];
-    params->dig_p4 = (int16_t)(buf[13] << 8) | buf[12];
-    params->dig_p5 = (int16_t)(buf[15] << 8) | buf[14];
-    params->dig_p6 = (int16_t)(buf[17] << 8) | buf[16];
-    params->dig_p7 = (int16_t)(buf[19] << 8) | buf[18];
-    params->dig_p8 = (int16_t)(buf[21] << 8) | buf[20];
-    params->dig_p9 = (int16_t)(buf[23] << 8) | buf[22];
+    params->dig_p1 = (uint16_t)(buf[7] << 8) | (uint16_t)buf[6];
+    params->dig_p2 = (int16_t)(buf[9] << 8) | (int16_t)buf[8];
+    params->dig_p3 = (int16_t)(buf[11] << 8) | (int16_t)buf[10];
+    params->dig_p4 = (int16_t)(buf[13] << 8) | (int16_t)buf[12];
+    params->dig_p5 = (int16_t)(buf[15] << 8) | (int16_t)buf[14];
+    params->dig_p6 = (int16_t)(buf[17] << 8) | (int16_t)buf[16];
+    params->dig_p7 = (int16_t)(buf[19] << 8) | (int16_t)buf[18];
+    params->dig_p8 = (int16_t)(buf[21] << 8) | (int16_t)buf[20];
+    params->dig_p9 = (int16_t)(buf[23] << 8) | (int16_t)buf[22];
+
+    params->dig_h1 = (uint8_t)buf[25];
+
+    // read 0xe1 - 0xe6
+    i2c_write_blocking(i2c_default, ADDR, &reg2, 1, true);
+    i2c_read_blocking(i2c_default, ADDR, buf, NUM_CALIB_PARAMS2, false);
+
+    params->dig_h2 = (int16_t)(buf[1] << 8) | (int16_t)buf[0];
+    params->dig_h3 = (uint8_t)buf[2];
+    params->dig_h4 = (int16_t)(buf[3] << 4) | (int16_t)(buf[4] & 0x0f);
+    params->dig_h5 = (int16_t)(buf[5] << 4) | (int16_t)(buf[4] >> 4 & 0x0f);
+    params->dig_h6 = (int8_t)buf[6];
+/*
+    printf("dig_t1 = 0x%02x, dig_t2 = 0x%02x, dig_t3 = 0x%02x\n", params->dig_t1, params->dig_t2, params->dig_t3);
+    printf("dig_p1 = 0x%02x, dig_p2 = 0x%02x, dig_p3 = 0x%02x\n", params->dig_p1, params->dig_p2, params->dig_p3);
+    printf("dig_p4 = 0x%02x, dig_p5 = 0x%02x, dig_p6 = 0x%02x\n", params->dig_p4, params->dig_p5, params->dig_p6);
+    printf("dig_p7 = 0x%02x, dig_p8 = 0x%02x, dig_p9 = 0x%02x\n", params->dig_p7, params->dig_p8, params->dig_p9);
+    printf("dig_h1 = 0x%02x, dig_h2 = 0x%02x, dig_h3 = 0x%02x\n", params->dig_h1, params->dig_h2, params->dig_h3);
+    printf("dig_h4 = 0x%02x, dig_h5 = 0x%02x, dig_h6 = 0x%02x\n", params->dig_h4, params->dig_h5, params->dig_h6);
+*/
 }
 
 #endif
@@ -212,8 +318,8 @@ int main() {
     stdio_init_all();
 
 #if !defined(i2c_default) || !defined(PICO_DEFAULT_I2C_SDA_PIN) || !defined(PICO_DEFAULT_I2C_SCL_PIN)
-    #warning i2c / bmp280_i2c example requires a board with I2C pins
-        puts("Default I2C pins were not defined");
+#warning i2c / bmp280_i2c example requires a board with I2C pins
+    puts("Default I2C pins were not defined");
     return 0;
 #else
     // useful information for picotool
@@ -238,14 +344,24 @@ int main() {
 
     int32_t raw_temperature;
     int32_t raw_pressure;
+    int32_t raw_humidity;
 
-    sleep_ms(250); // sleep so that data polling and register update don't collide
+    sleep_ms(250);  // sleep so that data polling and register update don't collide
     while (1) {
-        bmp280_read_raw(&raw_temperature, &raw_pressure);
+        bmp280_read_raw(&raw_temperature, &raw_pressure, &raw_humidity);
+/*
+        printf("Raw Pressure = %d\n", raw_pressure);
+        printf("Raw Temp = %d\n", raw_temperature);
+        printf("Raw Humidity = %d\n", raw_humidity);
+*/
         int32_t temperature = bmp280_convert_temp(raw_temperature, &params);
-        int32_t pressure = bmp280_convert_pressure(raw_pressure, raw_temperature, &params);
-        printf("Pressure = %.3f kPa\n", pressure / 1000.f);
+        int32_t pressure =
+            bmp280_convert_pressure(raw_pressure, raw_temperature, &params);
+        int32_t humidity =
+            bmp280_convert_humidity(raw_humidity, raw_temperature, &params);
+        printf("Pressure = %.2f hPa\n", pressure / 100.f);
         printf("Temp. = %.2f C\n", temperature / 100.f);
+        printf("Humidity. = %.2f %%\n", humidity / 1024.f);
         // poll every 500ms
         sleep_ms(500);
     }
